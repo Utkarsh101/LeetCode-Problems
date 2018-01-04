26830d95c81c8a6ca9a9ee48589e303eeef7ee64
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-04 00:26:47 -0500

    Add files via upload

--- Python/Odd Even Linked List.py
+++ Python/Odd Even Linked List.py
@@ -2,25 +2,24 @@ class Solution():
   def oddEvenList(self, head):
     
         try:  
-          copy1 = odd = head
+          odd = head
           copy2 = even = head.next
         
           while odd.next:
             if odd == None:
                 break
             odd.next = odd.next.next
+            prev = odd
             odd = odd.next
             if even.next == None:
                 break
             even.next = even.next.next
             even = even.next
             
-            
-            
-          while copy1.next:
-            copy1 = copy1.next
-          copy1.next = copy2 
-        
+          if odd:
+            odd.next = copy2
+          else:
+            prev.next = copy2
           return head
     
         except:
