134b1c08261ec821f6e4b24ba90fed3eb6eed028
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-06 21:52:03 -0500

    Add files via upload

---
+++ Python/Intersection of Two Arrays II.py
@@ -0,0 +1,22 @@
+'''
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+'''
+
+class Solution():
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        from collections import Counter
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+        return list((c1&c2).elements())
