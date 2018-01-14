459326a915816773aea9e6a9059b8eca58237a4f
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-14 12:18:41 -0500

    Add files via upload

---
+++ Python/Plus One.py
@@ -0,0 +1,15 @@
+'''
+Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+'''
+
+class Solution(object):
+  def plusOne(self, digits):
+    num = 0
+    for i in range(len(digits)):
+    	num += digits[i] * pow(10, (len(digits)-1-i))
+    return [int(i) for i in str(num+1)]
+            
