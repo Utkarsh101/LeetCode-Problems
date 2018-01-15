f21d10d779ad54a2646f484e8cbabf840f8eac6b
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-15 14:02:08 -0500

    Add files via upload

---
+++ Python/Shuffle an Array.py
@@ -0,0 +1,30 @@
+'''
+Shuffle a set of numbers without duplicates.
+'''
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        temp = self.nums[:]
+        random.shuffle(temp)
+        return temp
+        
