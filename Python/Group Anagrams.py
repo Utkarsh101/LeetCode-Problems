208e5f497f32f089b2011b19a0b5eb2becaaa63f
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-15 20:36:11 -0500

    Add files via upload

---
+++ Python/Group Anagrams.py
@@ -0,0 +1,23 @@
+'''
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+'''
+
+class Solution(object):
+  def groupAnagrams(self, strs):
+    d = {}
+    for w in strs:
+        key = tuple(sorted(w))
+        d[key] = d.get(key, []) + [w]
+    return d.values()
+
+        
+        
