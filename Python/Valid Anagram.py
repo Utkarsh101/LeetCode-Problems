3a63b213bedb59b5760b036c14fe3e686c534eee
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-01-18 11:04:29 -0500

    Add files via upload

---
+++ Python/Valid Anagram.py
@@ -0,0 +1,19 @@
+'''
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+'''
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        count1 = collections.Counter(s)
+        count2 = collections.Counter(t)
+        return count1==count2
+        
