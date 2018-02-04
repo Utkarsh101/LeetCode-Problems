48db4758d87e80e655553fcd9b2f14ec888a1646
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-02-04 14:43:05 -0500

    Add files via upload

---
+++ Python/Sum of Two Integers.py
@@ -0,0 +1,14 @@
+'''
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+'''
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        list1 = [a,b]
+        return sum(list1)
+        
