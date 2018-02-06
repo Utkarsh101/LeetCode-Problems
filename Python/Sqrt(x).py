dc2f2223a38ee48a431483c5412570a8e116f85f
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-02-06 15:52:56 -0500

    Add files via upload

---
+++ Python/Sqrt(x).py
@@ -0,0 +1,16 @@
+'''
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+x is guaranteed to be a non-negative integer.
+'''
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
+        
