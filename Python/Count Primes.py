f4608f775e4afa7800c29796374b86e050e8acd2
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-02-07 11:11:34 -0500

    Add files via upload

---
+++ Python/Count Primes.py
@@ -0,0 +1,16 @@
+'''
+Count the number of prime numbers less than a non-negative number, n
+'''
+
+class Solution(object):
+  def countPrimes(self, n):
+    if n <= 2:
+        return 0
+    res = [True] * n
+    res[0] = res[1] = False
+    for i in xrange(2, n):
+        if res[i] == True:
+            for j in xrange(2, (n-1)//i+1):
+                res[i*j] = False
+    return sum(res)
+        
