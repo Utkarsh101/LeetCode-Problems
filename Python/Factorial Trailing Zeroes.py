9f2c9324989a8ae9a45fb9ee10cef070974ea03d
Utkarsh Kumar Singh <kumarutkarsh61@gmail.com>
2018-02-07 11:01:27 -0500

    Add files via upload

---
+++ Python/Factorial Trailing Zeroes.py
@@ -0,0 +1,11 @@
+'''
+Given an integer n, return the number of trailing zeroes in n!.
+'''
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return 0 if n == 0 else n / 5 + self.trailingZeroes(n / 5)
